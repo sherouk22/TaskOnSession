889dfa1718eb12b491e1dc8bef07ca752c4eefa1
sherouk22 <sheroukmohy558@gmail.com>
2025-01-19 20:36:14 +0200

    Add project files.

---
+++ Assignment07C#OOP04/IOrderProcessor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment07C_OOP04
+{
+    public  interface IOrderProcessor
+    {
+        public void ProcessOrder();
+
+        decimal CalculateDiscount(decimal orderAmount);
+
+    }
+
+    public class OnlineOrderProcessor : IOrderProcessor
+    {
+        public void ProcessOrder()
+        {
+            Console.WriteLine("Processing online order.");
+        }
+
+        public decimal CalculateDiscount(decimal orderAmount)
+        {
+            return orderAmount * 0.10m; 
+        }
+
+        
+
+    }
+
+    public class InStoreOrderProcessor : IOrderProcessor
+    {
+
+        public void ProcessOrder()
+        {
+            Console.WriteLine("Processing in store order.");
+        }
+
+        public decimal CalculateDiscount(decimal orderAmount)
+        {
+            return orderAmount * 0.05m;
+        }
+
+
+
+
+
+    }
+
+
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string CustomerName { get; set; }
+        public decimal OrderAmount { get; set; }
+        public IOrderProcessor OrderProcessor { get; set; }
+
+        public IOrderProcessor GetOrderProcessor(string orderType)
+        {
+            if (orderType.Equals("Online", StringComparison.OrdinalIgnoreCase))
+            {
+                return new OnlineOrderProcessor();
+            }
+
+
+
+        }
+
+
+    }
+
+}
---
+++ Assignment07C#OOP04/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment07C_OOP04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
